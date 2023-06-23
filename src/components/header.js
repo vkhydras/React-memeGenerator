77d42d5db3f288c495bb39039abf2645cd2bc077
vkhydras <victorkimaru8@gmail.com>
2023-06-23 12:46:15 +0300

    firstCommit

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header className="header">
+            <img src="./img/troll-face.png" alt="troll-face" className="header-image"></img>
+            <h2 className="header-title">meme generator</h2>
+            <h4 className="project">React Course - project 3</h4>
+        </header>
+    )
+}
+
+export default Header;
